a5fed4290285bb211c1d766ed24a5190d1304a75
sarim <sarimk80.10p@gmail.com>
2020-01-31 11:12:22 +0500

    Initial Commit

---
+++ Learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning
+//
+//  Created by Sarim Khan on 31/01/2020.
+//  Copyright © 2020 Sarim Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
