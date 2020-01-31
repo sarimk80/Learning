381c590f4d8f8092e9759a057de69cd8403c1f0d
sarim <sarimk80.10p@gmail.com>
2020-01-31 12:38:02 +0500

    initial commit

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -9,8 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedIndex=0
+    @State private var nightMode=true
+    @State private var myText:String=""
+    @State private var initialValue=0
+    @State private var pageIndex=1
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView{
+            
+            Form {
+                List {
+                    Text("First Item").foregroundColor(.accentColor).font(.title)
+                    
+                    Toggle(isOn: $nightMode) {
+                        Text("Night Mode")
+                    }
+                    
+                    Button(action: {print("Click on button")}) {
+                        Text("Button")
+                    }
+                    TextField("Placeholder", text: $myText)
+                    Text("\(myText)")
+                    Stepper(value: $initialValue, in: 0...5) {
+                        Text("Select Age")
+                    }
+                    Text("\(initialValue)")
+                    
+                    
+                    VStack {
+                        Picker(selection: $pageIndex, label: Text("Picker")) {
+                            Text("1").tag(1)
+                            Text("2").tag(2)
+                        }.pickerStyle(SegmentedPickerStyle())
+                        
+                        Text("\(pageIndex)").font(.largeTitle).foregroundColor(.purple)
+                    }
+                    
+                    Image(systemName:"house").colorScheme(.light)
+                    
+                    
+
+
+                }
+            }.edgesIgnoringSafeArea(.top)
+            .navigationBarTitle("Swift UI",displayMode:.inline).navigationBarItems(trailing: Image(systemName:"house"))
+        }
+        
+        
     }
 }
 
